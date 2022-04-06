48fa73848aba3c329493db499e2f4b232022e1fb
apexphase <brandonbillings81@gmail.com>
2022-04-06 16:14:07 -0400

    2nd commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div class="container">
+        <div class="page-header">
+          <h1 style="color: green">Example Page Header</h1>
+        </div>
+      </div>
+    );
+  }
+}
